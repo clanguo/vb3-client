c892fdf3c090008a8e396feaceadef766d878921
clanguo <1642805102@qq.com>
2022-02-14 17:27:01 +0800

    取消sourceMap

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // devServer: {
   //   proxy: "http://localhost:3001/",
   // },
-  productionSourceMap: true,
+  productionSourceMap: false,
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
       args[0].title = "左耳东水良的个人博客";
